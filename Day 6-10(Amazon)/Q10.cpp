1f6c1947cb96a18555196dd9958795979d7d9a48
Harshdeep7r7 <86848077+Harshdeep7r7@users.noreply.github.com>
2022-01-10 21:59:02 +0530

    Create Q10.cpp

---
+++ Day 6-10(Amazon)/Q10.cpp
@@ -0,0 +1,31 @@
+int partition(char stuff[],int l,int r){
+
+        int index=l;
+
+        for(int i=l;i<r;i++){<br>
+            if(stuff[i]<=stuff[r]){
+                swap(stuff[index],stuff[i]);
+                index++;
+            }
+        }
+
+        swap(stuff[index],stuff[r]);
+        return index;
+    }
+
+    void qsort(char stuff[],int l,int r){
+
+        int p;
+        if(l<r){<br>
+            p=partition(stuff,l,r);
+            qsort(stuff,l,p-1);
+            qsort(stuff,p+1,r);
+        }
+
+    }
+
+void matchPairs(char nuts[], char bolts[], int n) {
+    qsort(nuts,0,n-1);
+    qsort(bolts,0,n-1);
+
+}
